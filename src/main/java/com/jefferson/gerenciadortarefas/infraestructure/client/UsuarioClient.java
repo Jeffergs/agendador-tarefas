c273ebbaa8a661edb3e8a843799e46044c266245
Jeffergs <jeffersongs1995@gmail.com>
2026-05-01 05:00:22 -0300

    Adicionadas configs de segurança

---
+++ src/main/java/com/jefferson/gerenciadortarefas/infraestructure/client/UsuarioClient.java
@@ -0,0 +1,15 @@
+package com.jefferson.gerenciadortarefas.infraestructure.client;
+
+import com.jefferson.gerenciadortarefas.infraestructure.busness.dto.UsuarioDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient (name = "usuário", url = "${usuario.url}")
+public interface UsuarioClient {
+    @GetMapping
+    UsuarioDTO buscarUsuarioPorEmail(@RequestParam("email") String email,
+                                     @RequestHeader("Authorization") String token);
+    }
